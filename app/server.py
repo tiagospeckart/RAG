9685fff9a39161b182fdf69549fcb2aba5bce6d0
Tiago Speckart <tiagospeckart@gmail.com>
2024-04-03 16:57:32 -0300

    feat: connection with remote LLM working

--- app/server.py
+++ app/server.py
@@ -1,30 +1,66 @@
+import os
+from typing import List, Union
+
+import openai
+from dotenv import load_dotenv
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import AzureChatOpenAI
 from langserve import add_routes
+from langserve.pydantic_v1 import BaseModel, Field
 
-app = FastAPI()
+# load environment variables
+load_dotenv()
 
-@app.get("/")
-async def redirect_root_to_docs():
-    return RedirectResponse("/docs")
+openai.api_key = os.getenv("AZURE_OPENAI_API_KEY")
+openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT")
+openai.api_version = "2023-03-15-preview"
+openai.api_type = "azure"
 
-@app.get("/test")
-async def HelloThere():
-    return {"message": "Hello World"}
+model = AzureChatOpenAI(
+    deployment_name="gpt-35-turbo",
+    temperature=0,
+    openai_api_version="2023-05-15"
+)
 
-class ModeloLinguagem:
+# FastAPI configuration
+app = FastAPI(
+    title="LangChain Server",
+    version="0.1",
+    description="Spin up a simple API server using Langchain's Runnable interfaces"
+)
 
-    def __init__(self):
-        pass
-    
-    def get_input_schema(self):
-        return {"type": "object", "properties": {"text": {"type": "string"}}, "required": ["text"]}
-    
+# Declare a chain + hardcoded system prompt
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful, professional assistant named Mag."),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+chain = prompt | model
+
+
+class InputChat(BaseModel):
+    """Input for the chat endpoint."""
+
+    messages: List[Union[HumanMessage, AIMessage, SystemMessage]] = Field(
+        ...,
+        description="The chat messages representing the current conversation."
+    )
+
+
+# Add routes
+add_routes(
+    app,
+    chain.with_types(input_type=InputChat),
+    enable_feedback_endpoint=True,
+    enable_public_trace_link_endpoint=True,
+    playground_type="chat",
+)
+
 if __name__ == "__main__":
-
-    modelo_linguagem = ModeloLinguagem()
-    add_routes(app, modelo_linguagem)
-    
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
