d203aa315b7fc50118aca380e12ee9498af375a8
Yuri Golfeto <yuri.golfeto@t-systems.com>
2024-04-03 10:07:57 -0300

    Update-Dev

--- app/server.py
+++ app/server.py
@@ -4,16 +4,27 @@ from langserve import add_routes
 
 app = FastAPI()
 
-
 @app.get("/")
 async def redirect_root_to_docs():
     return RedirectResponse("/docs")
 
+@app.get("/test")
+async def HelloThere():
+    return {"message": "Hello World"}
 
-# Edit this to add the chain you want to add
-add_routes(app, NotImplemented)
+class ModeloLinguagem:
 
+    def __init__(self):
+        pass
+    
+    def get_input_schema(self):
+        return {"type": "object", "properties": {"text": {"type": "string"}}, "required": ["text"]}
+    
 if __name__ == "__main__":
+
+    modelo_linguagem = ModeloLinguagem()
+    add_routes(app, modelo_linguagem)
+    
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
