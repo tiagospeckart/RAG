7caeb60f205cf09a41bb4ad0759bd096415e4740
Yuri Golfeto <yuri.golfeto@t-systems.com>
2024-04-18 15:35:42 -0300

    Feature-HistoryChat working Feature-Session_ID Working

--- app/server.py
+++ app/server.py
@@ -19,6 +19,12 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import AzureChatOpenAI
 from pydantic import BaseModel, Field
 from starlette.responses import RedirectResponse
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_history_aware_retriever
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 # load environment variables
 load_dotenv()
@@ -26,12 +32,11 @@ load_dotenv()
 # Model configuration
 openai.api_key = os.getenv("AZURE_OPENAI_API_KEY")
 openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT")
-openai.api_version = "2023-03-15-preview"
 openai.api_type = "azure"
 
 model = AzureChatOpenAI(
     deployment_name="gpt-35-turbo",
-    temperature=0.1,
+    temperature=0.08,
     openai_api_version="2023-05-15",
     http_client=httpx.Client(verify=False)
 )
@@ -85,13 +90,18 @@ conversation_memory = ConversationBufferWindowMemory(
     return_messages=True,
     output_key="answer",
     input_key="question",
+    k=4
 )
+chat_history = []
 # vectorstore.persist()
-retriever = vectorstore.as_retriever()
-system_instruction = "The assistant should provide detailed explanations."
+retriever = vectorstore.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 3},
+    )
+system_instruction = "The assistant should provide detailed explanations. also use the Chat_History to impruve your answer. reply with short answers"
 template = (
-    f"{system_instruction} "
-    "Combine the chat history and follow up question into "
+    f"system,{system_instruction} "
+    "Combine the chat history , documents and follow up question into "
     "a standalone question. Chat History: {chat_history}"
     "Follow up question: {question}"
 )
@@ -102,9 +112,9 @@ qa = ConversationalRetrievalChain.from_llm(
     return_source_documents=True,
     condense_question_prompt=condense_question_prompt,
     chain_type="stuff",
+    # memory = chat_history
     get_chat_history=lambda h : h,
-    memory=conversation_memory
-    
+    memory=conversation_memory,
 )
 chain = load_qa_chain(model, chain_type="refine")
 
@@ -113,10 +123,53 @@ chain = load_qa_chain(model, chain_type="refine")
 # print(response)
 # print("aqui e apos o retriever funcionar")
 # Examplo da Azure com qa
-chat_history = []
-result = {}
+
 # answser = qa({"question": query, "chat_history": chat_history})
 # print(answser)
+# Aware_retriever
+
+contextualize_q_system_prompt = """Given a chat history and the latest user question \
+which might reference context in the chat history, formulate a standalone question \
+which can be understood without the chat history. Do NOT answer the question, \
+just reformulate it if needed and otherwise return it as is."""
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+history_aware_retriever = create_history_aware_retriever(
+    model, retriever, contextualize_q_prompt
+)
+qa_system_prompt = """You are an assistant for question-answering tasks. \
+Use the following pieces of retrieved context to answer the question. \
+If you don't know the answer, just say that you don't know. \
+Use three sentences maximum and keep the answer concise.\
+
+{context}"""
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", qa_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+question_answer_chain = create_stuff_documents_chain(model, qa_prompt)
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+store = {}
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+)
+# Aware Retriever end
 # FastAPI configuration
 app = FastAPI(
     title="LangChain Server",
@@ -185,9 +238,21 @@ async def stream_runnable():
     pass
 @app.get(path + "/chat")
 async def chat_runnable(msg : str ):
-    answser = qa.invoke({"question": msg, "chat_history": conversation_memory})
-    # conversation_memory.asave_context(inputs=msg,outputs=answser)
-    return answser
+    # answer = qa.invoke({"question": msg})
+    answer = qa.invoke({"question": msg, "chat_history": conversation_memory})
+    # answer = qa.invoke({"question": msg, "chat_history": chat_history})
+    # chat_history.extend([HumanMessage(content=msg), answer["answer"]])
+    return answer
+
+@app.get(path + "/chatAware")
+async def chatAware(msg : str ,session_id):
+    answer = conversational_rag_chain.invoke(
+    {"input": msg},
+    config={
+        "configurable": {"session_id": session_id}
+    },
+    )["answer"]
+    return answer
 
 
 # Server init
